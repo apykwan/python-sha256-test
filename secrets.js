af058e7caa14244805af9b29bf593c813697f37d
apykwan <apykwan@hotmail.com>
2023-02-25 17:18:38 -0800

    typo

--- secrets.js
+++ secrets.js
@@ -1,3 +1,3 @@
 const password = process.env.DB_PASSWORD;
-const username = process.env.DB_USSERNAME;
+const username = process.env.DB_USERNAME;
 console.log(`${username} has set password: ${password}`);
